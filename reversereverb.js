519cf6084b768d3ea18b6f0407462cd540618742
John Garcia <166593810+johnalvarogarcia@users.noreply.github.com>
2024-04-13 16:36:45 +1200

    Create reversereverb.js

---
+++ reversereverb.js
@@ -0,0 +1,177 @@
+
+//Calling command "Focus Pro Tools, Center Cursor on Edit Window" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ck1w3h03r000tef10hc8eks5k',
+    props: {}
+});
+
+//Calling command "Copy" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xiomm00083n10ynrq3wsf',
+    props: {}
+});
+
+sf.ui.proTools.trackDuplicateSelected({
+    duplicateActivePlaylist: false,
+    duplicateAlternatePlaylists: false,
+    duplicateAutomation: false,
+    duplicateInserts: true,
+    duplicateSends: false,
+    duplicateGroupAssignments: true,
+    insertAfterLastSelectedTrack: false,
+});
+
+sf.wait({
+    intervalMs: 200,
+});
+
+//Calling command "Paste" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xiy5700093n10q9mqlgiv',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Move Edit Left" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xxyjt000z3n10zx3j4mnt',
+    props: {}
+});
+
+//Calling command "Selection - Double Edit" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xz54j00123n103ytq2j36',
+    props: {}
+});
+
+//Calling command "Selection - Double Edit" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xz54j00123n103ytq2j36',
+    props: {}
+});
+
+//Calling command "Selection - Double Edit" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xz54j00123n103ytq2j36',
+    props: {}
+});
+
+//Calling command "Selection - Double Edit" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckt90h5f100008a10ilhfochy#ckt4xz54j00123n103ytq2j36',
+    props: {}
+});
+
+//Calling command "Teezio’s Plugin Loader" from package "Teezio's Plugin Loader" (installed from user/pkg/version "uOwKfD26NbWKAWotin3dmnSne7B3/ckkcztgjh0000jt10i9gi61i5/clu7gkrdg000g2b10oq8x2543")
+sf.soundflow.runCommand({
+    commandId: 'user:cl9q4u6je0004e910m864jdzc:ckkczttld0001jt10m5krbbh9',
+    props: {
+        pluginName: "Little MicroShift",
+        pluginCatagory: "SoundToys",
+        pluginWidth: "(mono/stereo)",
+    }
+});
+
+//Calling command "Commit..." from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:cktqgh7kv00164k10b9xodbl0#cktqch2ft000e4k10q133rt8y',
+    props: {}
+});
+
+sf.ui.proTools.viewCloseCurrentPluginWindow();
+
+sf.ui.proTools.windows.whoseTitle.is("Commit Tracks").first.elementWaitFor();
+
+sf.ui.proTools.windows.whoseTitle.is("Commit Tracks").first.buttons.whoseTitle.is("OK").first.elementClick();
+
+sf.ui.proTools.confirmationDialog.children.whoseRole.is("AXStaticText").allItems[2].elementWaitFor({
+    waitType: "Disappear",
+});
+
+sf.ui.proTools.audioSuiteOpenPlugin({
+    category: "Reverb",
+    name: "ValhallaVintageVerb",
+});
+
+sf.ui.proTools.firstAudioSuiteWindow.audioSuiteSelectPreset({
+    presetMenuPath: ["4s"],
+});
+
+sf.ui.proTools.firstAudioSuiteWindow.audioSuiteSetOptions({
+    processingInputMode: "EntireSelection",
+    processingOutputMode: "CreateContinuousFile",
+});
+
+sf.ui.proTools.firstAudioSuiteWindow.buttons.whoseTitle.is("Analyze").first.elementClick();
+
+//Calling command "Close First AudioSuite plugin" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckydgt59600003110hy9vb7kb',
+    props: {}
+});
+
+//Calling command "Light 20" from package "Pro Tools" (installed from user/pkg/version "srAasovvDiQacRZ2mcId4RrOA8R2/ckp49i4j60000a2100yfwywgf/cltp2zdq50000i0101k7dxog6")
+sf.soundflow.runCommand({
+    commandId: 'user:ckp49i4j60000a2100yfwywgf:ckoybhyud000eua10qlvsfnie#ckoyah467001t3g1054osqvik',
+    props: {}
+});
+
